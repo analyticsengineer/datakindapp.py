eb7991e798e8b385d8d89c14978c484169c10d38
Anuoluwapo <83256563+anuoluwapods@users.noreply.github.com>
2022-12-13 22:31:09 +0100

    Update Alabama.py

--- ~/app/pages/Alabama.py
+++ ~/app/pages/Alabama.py
@@ -4,6 +4,6 @@ import streamlit as st
 
 st.write("City Of Alabama")
 
-df = pd.read_csv(r"Datasets/Alabama.csv")
-st.markdown("Alabama SNAP Program records")
+df = pd.read_csv(r'Datasets/Alabama.csv')
+st.markdown("Alabama SNAP Program records: ")
 st.dataframe(df)
