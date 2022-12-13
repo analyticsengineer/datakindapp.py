7785eef9789767fe170f85e9922bf15e9f7274dc
Anuoluwapo <83256563+anuoluwapods@users.noreply.github.com>
2022-12-13 22:58:15 +0100

    Update Alabama.py

--- ~/app/pages/Alabama.py
+++ ~/app/pages/Alabama.py
@@ -2,8 +2,35 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 
-st.write("City Of Alabama")
+st.header("City Of Alabama")
 
-df = pd.read_csv(r'Alabama.csv')
-st.markdown("Alabama SNAP Program records: ")
-st.dataframe(df)
+details = st.selectbox('Check Alabama city details in the SNAP Program',
+                       'Alabama City Map', 'Alabama potential gap rate',
+                       'Alabama SNAP data', 'Alabama Counties below federal poverty line',
+                       '% No of the people eligible for the SNAP Program in Alabama')
+
+
+if details == 'Alabama City Map':
+  st.write("You are currently viewing: ", details)
+  
+  
+if details == 'Alabama potential gap rate':
+  st.write("You are currently viewing: ", details)
+  
+if details == 'Alabama SNAP data':
+   st.write("You are currently viewing: ", details)
+   df = pd.read_csv(r'Alabama.csv')
+   st.markdown("Alabama SNAP Program records: ")
+   st.dataframe(df)
+    
+    
+if details == 'Alabama Counties below federal poverty line':
+  st.write("You are currently viewing: ", details)
+  
+  
+ if details == '% No of the people eligible for the SNAP Program in Alabama':
+  st.write("You are currently viewing: ", details)
+  df = 79
+  st.write("The % No of people living in Alabama Eligible for the SNAP Program is {}%".format(df))
+  
+  
