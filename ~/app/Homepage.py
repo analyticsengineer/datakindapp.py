346de7458a36e39dc7f48ee9423cb9077ea8c1fe
Anuoluwapo <83256563+anuoluwapods@users.noreply.github.com>
2022-12-13 21:23:28 +0100

    Update Homepage.py

--- ~/app/Homepage.py
+++ ~/app/Homepage.py
@@ -1,6 +1,16 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+from PIL import Image 
 
 
-st.title("SNAP & BDT Program Web App")
+# App Header
+col1, col2 = st.columns(2)
+image = Image.open('image.png')
+
+col1.markdown('''# **SNAP & BDT Program Web App**
+''')
+col1.write("A DATA KIND PROJECT")
+col2.image(image)
+
+
