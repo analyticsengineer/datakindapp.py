b1488de7d87d3a02c9d9f6f195e21dc583401d0e
Anuoluwapo <83256563+anuoluwapods@users.noreply.github.com>
2022-12-14 16:42:52 +0100

    Update Homepage.py

--- ~/app/Homepage.py
+++ ~/app/Homepage.py
@@ -13,7 +13,7 @@ col1.image(image)
 
 col1.write("This Web application consists of only states with counties below federal poverty line")
 
-col1.write("[![Project Page](https://img.icons8.com/material-outlined/24/undefined/github.png)](https://github.com/anuoluwapods/datakindapp.py) Project Source Code")
+col1.write("[![Project Page](https://img.icons8.com/material-outlined/24/undefined/github.png)](https://github.com/anuoluwapods/datakindapp.py)")
 
 
 
