9e6eda285eeb309a6ef89bf4758133f87c309cf8
Anuoluwapo <83256563+anuoluwapods@users.noreply.github.com>
2022-12-21 00:41:25 +0100

    Update Homepage.py

--- ~/app/Homepage.py
+++ ~/app/Homepage.py
@@ -11,7 +11,7 @@ image = Image.open('image.png')
 
 col1.image(image)
 
-col1.write("This Web application consists of only states with counties below federal poverty line")
+col1.write("This Web application consists of only states with counties below federal poverty line using the July 2022 Snap Data Records")
 
 col1.write("[![Project Page](https://img.icons8.com/material-outlined/24/undefined/github.png)](https://github.com/anuoluwapods/datakindapp.py)")
 col1.write("There are 31 states with Counties below the Federal Poverty Line")
